a4ca1c005bdea79f26db5349cab9e0c3ac118efb
Wong Shao Voon <shaovoon@yahoo.com>
2016-12-25 07:48:10 +0800

    Update License for combination.h

--- PermComb/permcomb/combination.h
+++ PermComb/permcomb/combination.h
@@ -1,36 +1,14 @@
-/*
-MIT License
+///////////////////////////////////////////////////////////////////////////////
+// combination.h header file
+//
+// Combination version 1.5
+// Copyright 2007 Wong Shao Voon
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+// See http://www.boost.org/libs/foreach for documentation
 
-Combination Library version 1.5
-
-Copyright (c) 2007 Wong Shao Voon
-
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without
-restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following
-conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-*/
-
-
-#ifndef __COMBINATION_H__
-#define __COMBINATION_H__
-
+#pragma once
 
 namespace boost
 {
@@ -390,5 +368,3 @@ void recursive_combination(RanIt nbegin, RanIt nend, int n_column,
 }
 
 }
-
-#endif
