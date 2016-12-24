2401953c9254ad87bd022820169bb511462223e5
Wong Shao Voon <shaovoon@yahoo.com>
2016-12-24 12:50:03 +0800

    Update License

--- PermComb/permcomb/concurrent_perm.h
+++ PermComb/permcomb/concurrent_perm.h
@@ -1,31 +1,12 @@
-/*
-MIT License
-
-Concurrent Permutation version 1.0.0
-
-Copyright (c) 2016 Wong Shao Voon
-
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without
-restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following
-conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-*/
+///////////////////////////////////////////////////////////////////////////////
+// concurrent_perm.h header file
+//
+// Concurrent Permutation version 0.1.0
+// Copyright 2016 Wong Shao Voon
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+// See http://www.boost.org/libs/foreach for documentation
 
 #pragma once
 
